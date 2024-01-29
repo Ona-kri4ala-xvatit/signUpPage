4558eb05eead79473b6d4b802cfe76c66bf9db23
Nijat Muradov <live4ever4@gmail.com>
2024-01-29 22:49:59 +0400

    Add index html/css/js

---
+++ js/index.js
@@ -0,0 +1,14 @@
+let form = document.forms.signUp
+let element = form.elements
+let ageElement = form.age
+
+console.log(`Age:`, ageElement.value)
+
+// if (passwordElement.value.length <= 5) {
+//   console.log('Не корректный пароль')
+// }
+
+// let cigaretesElement = elements.smoke
+// console.log(cigaretesElement.value)
+// console.log(cigaretesElement[0].value)
+// console.log(cigaretesElement[1].value)
